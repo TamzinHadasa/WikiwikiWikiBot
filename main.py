ebaadd8bb37014b00abca90105712662b01dbacb
Tamzin Hadasa Kelly <coding@tamz.in>
2022-02-19 03:25:15 -0500

    Adds basic readme and updates `main.py`.

--- main.py
+++ main.py
@@ -1,36 +1,25 @@
-"""Runs 'zinbot."""
+"""A lightweight framework for MediaWiki bots."""
 __author__ = "Tamzin Hadasa Kelly"
-__copyright__ = "Copyright 2021, Tamzin Hadasa Kelly"
+__copyright__ = "Copyright 2021-2022, Tamzin Hadasa Kelly"
 __license__ = "The MIT License"
 __email__ = "coding@tamz.in"
-__version__ = "1.4.2"
+__version__ = "0.1.0"
 
 import sys
 import time
 
 import api
 import antivandalism.massrollback
-import pagetriage.newpages
-
-
-def run() -> None:
-    """Run the bot's tasks that are currently approved / in trial."""
-    pagetriage.newpages.checkqueue()  # trial
 
 
 if __name__ == "__main__":
     print(f"RUNNING (version {__version__})")
     try:
-        if sys.argv[1] == "run":
-            while True:
-                run()
-       	        print("Run done. Sleeping.")
-                time.sleep(1800)
-        elif sys.argv[1] == "rollback":
+        if sys.argv[1] == "rollback":
             print(api.rollback(sys.argv[2]))
         elif sys.argv[1] == "massrollback":
             antivandalism.massrollback.main(file_name=sys.argv[2],
-                                           summary=sys.argv[3],
-                                           markbot=sys.argv[4] == 'markbot')
+                                            summary=sys.argv[3],
+                                            markbot=sys.argv[4] == 'markbot')
     except IndexError:
        sys.exit()
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Wikiwiki Wikibot: A lightweight solution to automated MediaWiki editing
+
+## Setup and configuration
+
+`pip install -r requirements.txt`
+
+Create a `config.py` file, following the instructions in `config_example.py`.
+
+## Running
+
+(When in the correct directory)
+`python main.py <script> [args]`
+
+See function documentation for available scripts.  @TODO: A proper list of
+options.
